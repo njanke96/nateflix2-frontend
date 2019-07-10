c7233c02e573f1df9581870e73b000da4759a33e
njanke96 <nathanjanke96@gmail.com>
2019-07-10 15:08:18 -0700

    navbar logic

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,91 @@
 import React from "react"
 
 export class Layout extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            // must be set when the user login status changes
+            loggedIn: false
+        }
+    }
+
     render() {
-        const serviceUrl = SERVICE_URL
         return (
-            <div className="notification is-primary">
-                <button className="delete"></button>
-                The service url is {serviceUrl}
+            <div>
+                <Navbar loggedIn={this.state.loggedIn}/>
             </div>
+        )
+    }
+}
+
+class Navbar extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            burgerMenuActive: false
+        }
+    }
+
+    toggleBurgerMenu () {
+        return (e) => {
+            this.setState((state) => {
+                return { burgerMenuActive: !state.burgerMenuActive }
+            })
+        }
+    }
+
+    render() {
+        let navMenuClass = "navbar-menu"
+        let burgerButtonClass = "navbar-burger burger" 
+        if (this.state.burgerMenuActive) {
+            navMenuClass += " is-active"
+            burgerButtonClass += " is-active"
+        }
+
+        return (
+            <nav className="navbar is-primary" role="navigation" aria-label="main navigation">
+                <div className="navbar-brand">
+                    <div className="navbar-item is-size-4">nateflix</div>
+                    
+                    {/* hidden button for dropdown on mobile*/}
+                    {
+                        this.props.loggedIn && 
+                        <a role="button" className={burgerButtonClass}
+                        aria-label="menu" 
+                        aria-expanded="false" 
+                        data-target="topNavbar"
+                        onClick={this.toggleBurgerMenu()}>
+                            <span aria-hidden="true"></span>
+                            <span aria-hidden="true"></span>
+                            <span aria-hidden="true"></span>
+                        </a>
+                    }
+                    
+                </div>
+
+                <div id="topNavbar" className={navMenuClass}>
+                    <div className="navbar-start">
+                        {
+                            this.props.loggedIn && [
+                                <a className="navbar-item">Home</a>,
+                                <a className="navbar-item">Movies</a>
+                            ]
+                        }
+                        
+                    </div>
+
+                    <div className="navbar-end">
+                        <div className="navbar-item">
+                            <div className="buttons">
+                                {
+                                    this.props.loggedIn &&
+                                    <a className="button is-secondary">Log out</a>  
+                                }        
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
         )
     }
 }
